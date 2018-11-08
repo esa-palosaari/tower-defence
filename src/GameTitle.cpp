607c21d91b189d83d650fdb51dc2bca08abc6f41
Katariina Tuovinen <katariina.tuovinen@aalto.fi>
2018-11-08 11:30:12 +0200

    Initial commit

---
+++ src/GameTitle.cpp
@@ -0,0 +1,36 @@
+#include "GameTitle.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+GameTitle::GameTitle() : nWindow(sf::VideoMode(1920, 1080), "GameTitle", sf::Style::None), title(), titleTexture() {
+    if(!titleTexture.loadFromFile("title.png")){
+        std::cout << "Opening title.png failed!" << std::endl;
+    }
+    else{
+        title.setTexture(titleTexture);
+    }
+}
+
+void GameTitle::Starta(){
+    while(nWindow.isOpen()){
+        manageEvents();
+        render();
+    }
+}
+
+void GameTitle::manageEvents(){
+    sf::Event event;
+    while(nWindow.pollEvent(event)){
+        if(event.type == sf::Event::KeyPressed || event.type == sf::Event::MouseButtonPressed){
+            nWindow.close();
+        }
+    }
+}
+
+void GameTitle::render(){
+    nWindow.clear();
+    nWindow.draw(title);
+    nWindow.display();
+}
+
+// /Users/oskartainio/Documents/Ohjelmointi/C++ -ohjelmointi/Projekti/GameTitle/GameTitle/ExternalLibraries/SFML/include/SFML
