2bef49017e363566090249b164e64544d75a60b0
Katariina Tuovinen <katariina.tuovinen@aalto.fi>
2018-11-08 11:27:42 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "GameTitle.hpp"
+#include <iostream>
+
+int main(){
+    std::cout<<"Game is starting.."<<std::endl;
+    GameTitle title;
+    title.Starta();
+    std::cout<<"Game is closed."<<std::endl;
+}
+
+// /Users/oskartainio/Documents/Ohjelmointi/C++ -ohjelmointi/Projekti/GameTitle/GameTitle/ExternalLibraries/SFML/include/SFML
