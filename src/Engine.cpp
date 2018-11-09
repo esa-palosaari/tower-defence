c1f850207a5db7ccabb06803bf320f217b2143a8
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-09 23:55:00 +0200

    Update Engine.cpp

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -7,34 +7,99 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+void Engine::StartEngine(){
+    loadMap(1);
+    Clock.restart();
+    UserGraphics graph(this); //Kannattaako lähettää olio vai tietorakenne?
+    graph.StartEngine();
+}
+
+void Engine::Update(sf::Time elapsedTime){
+    if(Level == 0 || FileLoaded){
+        CheckTimeout();
+    }
+    else{
+        for(auto& enemy:enemies){
+            auto EnemyPlacement = enemy.enemy.getPlacement(); //Enemy
+            enemy.Move(EnemyPlacement, elapsedTime; //Enemy
+            if(enemy.enemy.getPlacement().x >= 1984 && !enemy.isDead()){
+                if(HP>0){
+                    switch(enemy.getType()){
+                        case(Types::NPC::Type_4):
+                            HP=HP-1;
+                            break;
+                    }
+                }
+                enemy.Die();    //Enemy
+            }
+        }
+        std::sort(enemies.rbegin(), enemies.rend());
+        for(auto& enemy: enemies){
+            for(auto& tower : towers){
+                if(HP>0){
+                    tower.Shoot(enemy); //Tower
+                }
+            }
+        }
+        for(auto& tower : towers){
+            tower.ReTarget();   //Tower
+        }
+        for(std::vector<std::shared_ptr<Projectile>>iterator i=projectiles.begin(); i != projectiles.end(); ){
+            if(!i->get()->DoesntExist()){       //Projectile
+                i->get()->move(elapsedTime, enemies);   //Projectile
+                i++;
+            }
+            else{
+                i=projectiles.Remove(i);    //Projectile
+            }
+        }
+        if(Level<=3){
+            if(Clock.getElapsedTime().asMilliseconds() > Interval && enemiesSpawned < BaseLevels[Level-1][0]){
+                spawnEnemies(Types::NPC::Type_4);
+                enemiesSpawned++;
+                Clock.restart();
+            }
+        }
+        else{
+            if(Clock.getElapsedTime().asMilliseconds()>Interval && enemies.size() < MaxEnemies){
+                spawnEnemies(Types::NPC::Type_4);
+                Clock.restart();
+            }
+        }
+        if(enemies.size() == MaxEnemies && HP > 0){
+            CheckTimeOut();
+        }
+    }
+}
+
 bool Engine::isAllDead(){
     if(enemies.size() < 1){
-        return true;
+        return false;
     }
     for(int i=0; i<enemies.size(); i++){
         if(!enemies[i].isDead()){   //Enemy class: isDead()
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 void Engine::EndGame(){
     if(!GameEnd){
         GameEnd=true;
     }
 }
-void Engine::saveGame(std::string targetPath){  
+void Engine::saveGame(std::string targetPath){
     std::ofstream out;
     out.open(targetPath, std::ios:app);
-    out<<points<<';'<<round<<';'<<HP<<';';
-    std::ostringstream archive_streamT;
-    boost::archive::text_oarchive archiveT(archive_streamT);
-    archiveT << towers;
-    std::string outbound_dataT_ = archive_streamT.str();
-    out<<outbound_dataT_;
+    out<<money<<';'<<Level<<';'<<HP<<';';
+    std::ostringstream AstreamT;
+    boost::archive::text_oarchive ArchiveT(AstreamT);
+    ArchiveT << towers;
+    std::string OdataT = AstreamT.str();
+    out<<OdataT;
     out.close();
 }
-void Engine::watchForTimeout(){
+void Engine::CheckTimeOut(){
     if(isAllDead() && !TimeOut){
         TimeOut=true;
         TimeOutClock.restart();
@@ -55,7 +120,7 @@ void Engine::spawnEnemies(Types::NPC type){
 }
 void Engine:spawnEnemies(Types::NPC type, float x, float y, int flag, float distance){
     Enemy&& temp = Enemy(type, SpawnNumber, flag, distance);
-    temp.initSprite(0.f, 160.f);
+    temp.initSprite(x, y);
     enemies.push_back(temp);
     SpawnNumber++;
 }
@@ -64,12 +129,36 @@ void Engine::spawnTower(Types::NPC type, float x, float y){
     temp.initSprite();
     towers.push_back(temp);
 }
-void Engine::StartEngine(){
-    loadMap(1);
-    clock.restart();
-    UserGraphics graph(this); //Kannattaako lähettää olio vai tietorakenne?
-    graph.StartEngine();
+void Engine::loadMap(int Id){
+    std::string MPath = "Kartta"+std::to_string(Id)+".txt";
+    std::ifstream MFile(MPath);
+    std::string Value;
+    int i=0;
+    int level[510];
+    while(std::getline(MFile,Value,',')){
+        level[i]=std::stoi(Value);
+        i++;
+    }
+    if(!map.load("TileRange.png", sf::Vector2u(64,64), level, 30, 17)){
+        std::cout<<"Can not load the map."<<std::endl;
+    }
 }
+void Engine::LevelUp(){
+    enemiesSpawned = 0;
+    if(round < 3){
+        MaxEnemies= BaseLevels[Level][0];
+    }
+    else{
+        MaxEnemies = Level * 2;
+    }
+    money +=500;
+    Level++;
+    enemies.reserve(MaxEnemies);
+    std::cout<<"New level starting!"<<std::endl;
+}
+
+
+
 
 
 
