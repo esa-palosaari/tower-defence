e544d541916f1fbd1fb8cd04b96879abbf4e4bc4
Katariina Tuovinen <Kata@Katariinas-MacBook-Air.local>
2018-11-11 17:50:56 +0200

    loadmap function update

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+// Starts the engine, loads the game map from file, starts the clock and calls UserGraphics
 void Engine::StartEngine(){
     loadMap(1);
     Clock.restart();
@@ -17,36 +18,39 @@ void Engine::StartEngine(){
 void Engine::Update(sf::Time elapsedTime){
     if(Level == 0 || FileLoaded){
         CheckTimeout();
-    }
-    else{
+    } else {
         for(auto& enemy:enemies){
             auto EnemyPlacement = enemy.enemy.getPlacement(); //Enemy
-            enemy.Move(EnemyPlacement, elapsedTime; //Enemy
+            enemy.Move(EnemyPlacement, elapsedTime); //Enemy
             if(enemy.enemy.getPlacement().x >= 1984 && !enemy.isDead()){
                 if(HP>0){
                     switch(enemy.getType()){
                         case(Types::NPC::Type_4):
-                            HP=HP-1;
+                            HP--;
                             break;
                     }
                 }
                 enemy.Die();    //Enemy
             }
         }
+
         std::sort(enemies.rbegin(), enemies.rend());
-        for(auto& enemy: enemies){
+
+        for(auto& enemy : enemies){
             for(auto& tower : towers){
                 if(HP>0){
                     tower.Shoot(enemy); //Tower
                 }
             }
         }
+
         for(auto& tower : towers){
             tower.ReTarget();   //Tower
         }
+
         for(std::vector<std::shared_ptr<Projectile>>iterator i=projectiles.begin(); i != projectiles.end(); ){
-            if(!i->get()->DoesntExist()){       //Projectile
-                i->get()->move(elapsedTime, enemies);   //Projectile
+            if(!i->get()->DoesntExist()){       // Projectile
+                i->get()->move(elapsedTime, enemies);   // Projectile
                 i++;
             }
             else{
@@ -72,94 +76,115 @@ void Engine::Update(sf::Time elapsedTime){
     }
 }
 
+// Checks if all enemies are dead
 bool Engine::isAllDead(){
     if(enemies.size() < 1){
         return false;
     }
-    for(int i=0; i<enemies.size(); i++){
+    for(int i = 0; i < enemies.size(); i++){
         if(!enemies[i].isDead()){   //Enemy class: isDead()
             return false;
         }
     }
     return true;
 }
+
+
+// Ends the game
 void Engine::EndGame(){
     if(!GameEnd){
-        GameEnd=true;
+        GameEnd = true;
     }
 }
+
+
+// Saves the game into a .txt - file
 void Engine::saveGame(std::string targetPath){
     std::ofstream out;
     out.open(targetPath, std::ios:app);
-    out<<money<<';'<<Level<<';'<<HP<<';';
+    out << money << ';' << Level << ';' << HP << ';';
     std::ostringstream AstreamT;
     boost::archive::text_oarchive ArchiveT(AstreamT);
     ArchiveT << towers;
     std::string OdataT = AstreamT.str();
-    out<<OdataT;
+    out << OdataT;
     out.close();
 }
+
+
 void Engine::CheckTimeOut(){
     if(isAllDead() && !TimeOut){
-        TimeOut=true;
+        TimeOut = true;
         TimeOutClock.restart();
     }
-    if(TimeOutClock.getElapsedTime().asSeconds()>TimeOutTime && TimeOut){
+
+    if(TimeOutClock.getElapsedTime().asSeconds() > TimeOutTime && TimeOut){
         FileLoaded = false;
         enemies.clear();
         newLevel();
-        TimeOut=false;
-        TimeOutTime=10;
+        TimeOut = false;
+        TimeOutTime = 10;
     }
 }
+
 void Engine::spawnEnemies(Types::NPC type){
     Enemy&& temp = Enemy(type, SpawnNumber, 0, 0);
     temp.initSprite(0.f, 160.f);
     enemies.push_back(temp);
     SpawnNumber++;
 }
+
+
 void Engine:spawnEnemies(Types::NPC type, float x, float y, int flag, float distance){
     Enemy&& temp = Enemy(type, SpawnNumber, flag, distance);
     temp.initSprite(x, y);
     enemies.push_back(temp);
     SpawnNumber++;
 }
+
+
 void Engine::spawnTower(Types::NPC type, float x, float y){
     Tower&& temp = Tower(this, type, x, y);
     temp.initSprite();
     towers.push_back(temp);
 }
-void Engine::loadMap(int Id){
-    std::string MPath = "Kartta"+std::to_string(Id)+".txt";
-    std::ifstream MFile(MPath);
-    std::string Value;
-    int i=0;
-    int level[510];
-    while(std::getline(MFile,Value,',')){
-        level[i]=std::stoi(Value);
+
+// Loads the map from .txt - file into array
+void Engine::loadMap(int id){
+  std::string map_path = "map" + std::to_string(id) + ".txt";
+  std::ifstream map_file(map_path);
+  std::string pixel;
+
+  int i = 0;
+  int pixels[510];
+
+  if(map_file.is_open()) {
+    while(std::getline(map_file, pixel, ',')){
+        pixels[i] = std::stoi(pixel);
         i++;
     }
-    if(!map.load("TileRange.png", sf::Vector2u(64,64), level, 30, 17)){
-        std::cout<<"Can not load the map."<<std::endl;
-    }
+    map_file.close();
+  }
+
+// Create the tilemap from the level definition
+  TileMap map;
+  if(!map.load("TileRange.png", sf::Vector2u(64, 64), pixels, 30, 17)){
+      std::cout << "Can not load the map." << std::endl;
+  }
+
 }
+
+// Updates the player to a next level
 void Engine::LevelUp(){
     enemiesSpawned = 0;
     if(round < 3){
-        MaxEnemies= BaseLevels[Level][0];
-    }
-    else{
+        MaxEnemies = BaseLevels[Level][0];
+    } else {
         MaxEnemies = Level * 2;
     }
-    money +=500;
+
+    money += 500;
     Level++;
     enemies.reserve(MaxEnemies);
-    std::cout<<"New level starting!"<<std::endl;
+    std::cout << "New level starting!" << std::endl;
 }
-
-
-
-
-
-
-
--- src/Engine.hpp
+++ src/Engine.hpp
@@ -15,70 +15,64 @@ public:
     void StartEngine();
     void Update(sf::Time TimeElapsed);
     int TimeToNextRound() {return TimeOutTime - TimeOutClock.getElapsedTime().asSeconds();}
-    
+
     int getMoney() const {return money;};
     void loseMoney(int money) {money -= money;}
     int getLevel() const {return Level;}
     int getHP() const {return HP;}
-    
+
     bool isTimeOut() {return TimeOut;}
     void resetTimeOut() {TimeOutTime = 0;}
     void increaseTimeOut(TimeOutTime += time;)
     bool isAllDead()
     bool isEnd() {return GameEnd;}
-    
+
     void EndGame();
     void saveGame(std::string filetarget);
-    
+
     void spawnTower(Types::NPC type, float x, float y);
     void spawnEnemies(Types::NPC type);
     void spawnEnemies(Types::NPC type, float x, float y, int flag, float distance)
-    
+
     void setMoney(int startmoney) {money = startmoney;}
     void setHP(int startHP) {HP = startHP;}
     void setLevel(int startLevel) {Level = startLevel;}
     void setGameTag(std::string UserTag) {GameTag = UserTag;}
-    
+
     TileMap map;
     std::vector<Enemy> enemies;
     std::vector<Tower> towers;
     std::vector<Projectile> projectiles;
-    
-    
+
+
 private:
-    //Player's statistics
+    // Player's statistics
     std::string GameTag;
     int Level = 0;
     int HP = 100;
     int money = 1000;
-    
-    //Enemy information
+
+    // Enemy information
     float spawnPointX = 0;
     float spawnPointY = 0;
     int SpawnNumber = 0;
-    int MaxEnemies=0;
+    int MaxEnemies = 0;
     int enemiesSpawned = 0;
     int BaseLevels[3][1] = {{1},{2},{3}};
-    
-    //Game's state information
+
+    // Game's state information
     int Interval = 1000;
     int TimeOutTime = 10;
     bool GameEnd = false;
     bool TimeOut = true;
     bool FileLoaded = false;
-    
-    //SFML properties
+
+    // SFML properties
     sf::Clock TimeOutClock;
     sf::Clock Clock;
-    
-    //Required functions
+
+    // Required functions
     void loadMap(int Id);
     void LevelUp();
     void CheckTimeOut();
 }
-
-
-
-
-
-
