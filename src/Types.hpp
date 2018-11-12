d6db17174f55450079d03b73f5980ca207ff9b06
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-12 11:45:43 +0200

    Update Types.hpp

--- src/Types.hpp
+++ src/Types.hpp
@@ -9,9 +9,9 @@ class Types
             Type_1, // Tower_1
             Type_2, // Tower_2
             Type_3, // Tower_3
-            Type_4, // Enemy_1
-            Type_5, // Enemy_2
-            Type_6, // Enemy_3
+            Slow, // Enemy_1
+            Medium, // Enemy_2
+            Fast, // Enemy_3
             type_7, // Projectile_1
             Type_8, // Projectile_2
             Type_9  // Projectile_3
