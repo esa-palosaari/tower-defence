e75d8a1654b7c423a695a053a878f2027a856963
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-12 15:02:18 +0200

    Update Types.hpp

--- src/Types.hpp
+++ src/Types.hpp
@@ -9,9 +9,9 @@ class Types
             Type_1, // Tower_1
             Type_2, // Tower_2
             Type_3, // Tower_3
-            Slow, // Enemy_1
-            Medium, // Enemy_2
-            Fast, // Enemy_3
+            Slow, // Slow speed, high HP. "Tank"
+            Medium, // Medium speed, medium HP. "Pasi"
+            Fast, // High speed, low HP. "Bike"
             type_7, // Projectile_1
             Type_8, // Projectile_2
             Type_9  // Projectile_3
