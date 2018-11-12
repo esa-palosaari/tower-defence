27443f9ee7e9e2ccfbde7cd81fcba3327f04b09f
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-12 11:21:14 +0200

    Update Enemy.hpp

--- src/Enemy.hpp
+++ src/Enemy.hpp
@@ -2,27 +2,28 @@
 #define Enemy_hpp
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <vector>
-#include "Engine.hpp"
+#include <iostream>
 #include "Types.hpp"
-
-// taytyyko engine forward declaroida?
 
 class Enemy {
     public:
         Enemy {Engine* game, Types::NPC type, int x, int y);
         Enemy() {};
-        // tarvitaanko muita funktioita?
         move();
         getHit();
         EShoot();
         checkDistance();
         hit();
     private:
-        types::NPC type;
-        int HP;
-        int moveRate;
+        Types::NPC type;
+        int Hp;
+        bool Dead=false;
+        int Muuttuja=0; //Move-funktiossa käytettävä.
+        float Speed;
+        float LengthTravelled=0;
+        sf::Vector2f Forward; //Move-function: Moves object 'forward'
+        sf::Vector2f UpDown; //Move-function: Moves object down or up depending on position.
+        sf::Vector2f Hold; //Move-function: Stops object from moving.
         int DMG;
         int x;
         int y;
