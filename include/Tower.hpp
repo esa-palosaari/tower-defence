cfc2eadafadd22c8b3e510fa8ffec13cb6ee5915
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-17 09:40:35 +0200

    Update Tower.hpp

--- include/Tower.hpp
+++ include/Tower.hpp
@@ -10,32 +10,45 @@
 class Engine; // taytyyko engine forward declaroida?
 
 class Tower {
-    public:
-        Tower {Engine* game, Types::NPC type, float x, float y);
-        Tower() {};
-        // implementoi funktiot 
-        sf::Sprite tower;
-        sf::CircleShape circle;
-        bool textureInit = false;
-        void initializeTower();
-        void initializeTexture();
-        bool checkRange(Enemy const enemy);
-        void Shoot(Enemy& enemy);
-        void setAngle(Enemy const enemy);
-        void ReTarget(); // kutsuu checkrangea, in range valitsee pisimmalle vaeltaneen enemyn
-        int getRange(); // etsii 
-        int getFirerate();
-        int getPrice();
-        sf::Sprite getTower();
-        Types::NPC getType() const;
-    private:
-        Types::NPC type;
-        Engine* game; // ?
-        float range;
-        int firerate;
-        int damage;
-        int price;
-        float x;
-        float y; // tarvitaanko tata vai korvataanko kahdella muuttujalla?
+public:
+    Tower {Engine* game, Types::NPC type, float x, float y);
+    void InitializeSprite();
+    void InitializeTexture();
+    void Shoot(Enemy& enemy);
+    void setTimePause(float Time) { TimePause = Time; }
+    void ReTarget();
+        //Getters
+    int getRange();
+    int getFirerate();
+    int getPrice();
+    sf::Sprite getTower();
+    Types::NPC getType() const;
+        //Values declared in public.
+    sf::Sprite tower;
+    sf::CircleShape circle;
+    bool textureInitialize = false;
+            
+    template <typename Archive>
+    void serialize(Archive& statistics, const unsigned int version){
+        ValueType & type;
+        ValueType & x;
+        ValueType &y;
+        ValueType &Damage;
+        ValueType &range;
+        ValueType &firerate;
+    }
+private:
+    void AimAngle(Enemy const enemy);
+    bool inRange(Enemy const enemy);
+    Types::NPC type;
+    Engine* engine;
+    float Range;
+    int Firerate;
+    int DMG;
+    int Price;
+    int TargetID;
+    float x;
+    float y;
+    float TimePause = 0;
 };
 #endif
