5b71bd43cfcb7deb7ebd8c090ae234e8e59b94ae
Oskar Tainio <oskar.tainio@aalto.fi>
2018-11-18 20:00:44 +0200

    Update Types.hpp

--- include/Types.hpp
+++ include/Types.hpp
@@ -7,14 +7,11 @@ class Types
         enum NPC
         {
             Machinegun, // High fire rate, low damage. Impact damage on single target.
-            Flame, // Medium fire rate, medium damage. Possible ray effect.
-            Launcher, // Low fire rate, high damage. AoE.
+            Flamethrower, // Medium fire rate, medium damage. Possible ray effect.
+            Rocketlauncher, // Low fire rate, high damage. AoE.
             Slow, // Slow speed, high HP. "Tank"
             Medium, // Medium speed, medium HP. "Pasi"
             Fast, // High speed, low HP. "Bike"
-            Bullet, // Projectile type 1
-            Flame, // Projectile type 2
-            Rocket // Projectile type 3
         };
 
 };
