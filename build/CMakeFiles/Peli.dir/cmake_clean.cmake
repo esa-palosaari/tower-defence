ac78c8d8927df7ff85de3eccd0bd43d6cf02b070
Arsi Ikäheimonen <arsi.ikaheimonen@aalto.fi>
2018-12-05 13:23:31 +0200

    new GameTitle picture

---
+++ build/CMakeFiles/Peli.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Peli.dir/src/GameTitle.o"
+  "CMakeFiles/Peli.dir/src/GameMenu.o"
+  "CMakeFiles/Peli.dir/src/main.o"
+  "CMakeFiles/Peli.dir/src/Engine.o"
+  "CMakeFiles/Peli.dir/src/Enemy.o"
+  "CMakeFiles/Peli.dir/src/Tower.o"
+  "CMakeFiles/Peli.dir/src/Projectile.o"
+  "CMakeFiles/Peli.dir/src/UserGraphics.o"
+  "Peli.pdb"
+  "Peli"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Peli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
