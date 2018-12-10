1a2ad7db0dc73bbfd05441b2c2130199ece29971
Katariina Tuovinen <Kata@dhcp-87-193.eduroam.aalto.fi>
2018-12-10 14:29:19 +0200

    Documentation added

--- include/Enemy.hpp
+++ include/Enemy.hpp
@@ -20,27 +20,27 @@ class Enemy {
         bool CheckDead(); //Checks if enemy is dead or not
         void setTimePause(float Time);
         void InitializeTexture();
-        
+
         bool TextureInitialize = false;
         bool operator<(const Enemy& E) const{return (LengthTravelled < E.LengthTravelled);} //Operator overloading for sorting different enemies when calculating which one to shoot at.
-    
+
         sf::Sprite enemy;
         sf::RectangleShape HealthBar;
         sf::RectangleShape base;
         sf::Clock clock;
-	int Muuttuja=0; //Needed for pathfinding and setting rotation
+	      int Muuttuja = 0; //Needed for pathfinding and setting rotation
 
     private:
         Types::NPC type; //Type of enemy
         int Hp; //Healthpoints
-	int HpBase;
+	      int HpBase;
         int IdNum;  //Identification number, check engine cpp.
         int Bounty; //Amount of money enemy drops when it dies.
         float Speed; //Enemy moves this fast.
         float LengthTravelled = 0; //Distance from spawn
         float PauseTime = 0;
-	float LevelModifier;
-        bool Dead=false; //If object is dead or not
+	      float LevelModifier;
+        bool Dead = false; //If object is dead or not
         sf::Vector2f Forward; //Move-function: Moves object 'forward'
         sf::Vector2f UpDown; //Move-function: Moves object down or up depending on position.
         sf::Vector2f Hold; //Move-function: Stops object from moving.
