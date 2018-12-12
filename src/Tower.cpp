a9b28ce5c2b280695d1d662cef036b92879893e3
Katariina Tuovinen <Kata@dhcp-87-250.eduroam.aalto.fi>
2018-12-12 11:37:50 +0200

    comments added

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -14,7 +14,7 @@ Tower::Tower(Engine* engine, Types::NPC type, float x, float y) : engine(engine)
         DMG = 100;
         Price = 500;
         break;
-    
+
         case(Types::NPC::Flamethrower):
         Range =  275;
         Firerate = 45;
@@ -31,15 +31,14 @@ Tower::Tower(Engine* engine, Types::NPC type, float x, float y) : engine(engine)
     }
 }
 
-//SFML initialization.
+//Initializes tower's sprite
+void Tower::InitializeSprite() {
 
-void Tower::InitializeSprite()      //Initializes tower's sprite
-{
     tower.setPosition(x,y);
     tower.setOrigin(32.f,32.f);
-	
-	infopanel.setPosition(1690,550);
-	infopanel.setScale(0.5f,0.26f);
+
+  	infopanel.setPosition(1690,550);
+  	infopanel.setScale(0.5f,0.26f);
 
     circle.setPosition(x,y);
     circle.setOrigin(Range,Range);
@@ -47,29 +46,25 @@ void Tower::InitializeSprite()      //Initializes tower's sprite
     circle.setFillColor(sf::Color(125, 125, 125, 255));
 }
 
-void Tower::InitializeTexture() //Initializes texture for tower.
-{
+//Initializes texture for tower.
+void Tower::InitializeTexture() {
     textureInitialize = true;
 }
 
-//Getters
-int Tower::getRange()
-{
+// Getters
+int Tower::getRange(){
     return Range;
 }
 
-int Tower::getPrice()
-{
+int Tower::getPrice(){
     return Price;
 }
 
-Types::NPC Tower::getType () const
-{
+Types::NPC Tower::getType () const{
     return type;
 }
 
-int Tower::getFirerate()
-{
+int Tower::getFirerate(){
     return Firerate;
 }
 
@@ -77,67 +72,73 @@ int Tower::getDMG(){
 	return DMG;
 }
 
-sf::Sprite Tower::getTower()    //Sprite is named 'tower'
-{
+//Sprite is named 'tower'
+sf::Sprite Tower::getTower(){
     return tower;
 }
 
-//Shooting phase functions.
-
-void Tower::ReTarget(){ //Called from Engine.cpp to every tower after every tower has fired per frame.
+// Shooting phase functions.
+// Called from Engine.cpp to every tower after every tower has fired per frame.
+void Tower::ReTarget(){
     TargetID = -1;
 }
 
-void Tower::Shoot(Enemy& enemy)     //Called from Engine.cpp
-{
-    if (TargetID == -1 && inRange(enemy)){  //TargetID is -1, when there is not target, butchanges when there is potential enemy in range.
+
+//
+void Tower::Shoot(Enemy& enemy) {
+
+    //TargetID is -1, when there is no target, but changes when there is potential enemy in range.
+    if (TargetID == -1 && inRange(enemy)){
         TargetID = enemy.getIdNum();
     }
-    if (TargetID == enemy.getIdNum() && inRange(enemy)) //Checks if targeted enemy has same targetID compared to turret's.
-    {
-        AimAngle(enemy);    //If so, turret aims at the enemy.
+
+    // Checks if targeted enemy has same targetID compared to turret's.
+    if (TargetID == enemy.getIdNum() && inRange(enemy)){
+        AimAngle(enemy);    // If so, turret aims at the enemy.
     }
-    if (TargetID == enemy.getIdNum() && inRange(enemy) && clock.getElapsedTime().asMilliseconds() - TimePause > Firerate){  //Lastly, turret checks whether enough time has passed since last shot. Time hast to be more than firerate.
-        clock.restart();    //clock is restarted, which means that shots are fired.
-        TimePause = 0;      //TimePause is resetted.
-        if(type==Types::NPC::Machinegun){       //Depending on each turrets type, different kind of projectile is fired.
-            //std::shared_ptr<MachinegunProjectile> projectile = std::make_shared<MachinegunProjectile>(DMG, x, y, type, enemy.getIdNum());   //Projectiles are tracked using shared pointers.
-            engine->projectiles.push_back(std::make_shared<MachinegunProjectile>(MachinegunProjectile(DMG, x, y, type, enemy.getIdNum()))); //Projectiles vector is updated.
-			shootSound.play();	
-        }
-        else if(type == Types::NPC::Flamethrower){
-            //std::shared_ptr<FlameProjectile> projectile = std::make_shared<FlameProjectile>(DMG, x, y, type, enemy.getIdNum());
+
+    // Lastly, turret checks whether enough time has passed since last shot. Time hast to be more than firerate.
+    if (TargetID == enemy.getIdNum() && inRange(enemy) && (clock.getElapsedTime().asMilliseconds() - TimePause) > Firerate){
+        clock.restart();    // clock is restarted, which means that shots are fired.
+        TimePause = 0;      // TimePause is resetted.
+        if(type == Types::NPC::Machinegun){       // Depending on each turrets type, different kind of projectile is fired.
+            //Projectiles vector is updated.
+            engine->projectiles.push_back(std::make_shared<MachinegunProjectile>(MachinegunProjectile(DMG, x, y, type, enemy.getIdNum())));
+			      shootSound.play();
+        } else if (type == Types::NPC::Flamethrower){
             engine->projectiles.push_back(std::make_shared<FlameProjectile>(FlameProjectile(DMG, x, y, type, enemy.getIdNum())));
-			shootSound.play();
-        }
-        else if(type == Types::NPC::Rocketlauncher){
-            //std::shared_ptr<RocketProjectile> projectile = std::make_shared<RocketProjectile>(DMG, x, y, type, enemy.getIdNum());
+			      shootSound.play();
+        } else if(type == Types::NPC::Rocketlauncher){
             engine->projectiles.push_back(std::make_shared<RocketProjectile>(RocketProjectile(DMG, x, y, type, enemy.getIdNum())));
-			shootSound.play();
+			      shootSound.play();
         }
     }
 }
 
-bool Tower::inRange(Enemy enemy)    //This checks if targeted enemy is in range. Called from tower's Shoot()-function.
-{
+
+// Checks if targeted enemy is in range. Called from tower's Shoot()-function.
+bool Tower::inRange(Enemy enemy){
     if (enemy.CheckDead()){
          return false;
     }
+
     float XLength = abs(enemy.enemy.getPosition().x - tower.getPosition().x);
     float YLength = abs(enemy.enemy.getPosition().y - tower.getPosition().y);
-    return sqrt(pow(XLength,2) + pow(YLength,2)) < Range;
+    return sqrt(pow(XLength, 2) + pow(YLength, 2)) < Range;
 }
-void Tower::AimAngle(Enemy enemy)   //This alters tower's sprite to face enemy's direction. Called from tower's Shoot() -function.
-{
+
+// Alters tower's sprite to face enemy's direction. Called from tower's Shoot() -function.
+void Tower::AimAngle(Enemy enemy) {
     sf::Vector2f SightAngle = -tower.getPosition() + enemy.enemy.getPosition();
     double Angle = 2 * 3.14159265 - atan2(SightAngle.x, SightAngle.y);
     Angle = Angle * 360 / (2*3.14159265);
     tower.setRotation(Angle + 180);
 }
 
+// Alters tower's level, dmg, range and money
 void Tower::setTowerLevel(){
-	DMG=DMG*2;
-	Range= Range+5;
+	DMG = DMG * 2;
+	Range += 5;
 
 	switch(this->TowerLevel){
 		case(1):
@@ -150,6 +151,8 @@ void Tower::setTowerLevel(){
 			engine->loseMoney(800);
 			break;
 	}
+
 	TowerLevel++;
 	upgradeSound.play();
+  
 }
