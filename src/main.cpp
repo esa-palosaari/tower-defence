91ee5db77544538b687f516fe7de809152d84535
Katariina Tuovinen <Kata@dhcp-87-250.eduroam.aalto.fi>
2018-12-12 11:00:29 +0200

    comments added

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,23 @@
 #include <string>
 
 int main(){
-	int exitStatus;	
+
+	int exitStatus;
 	std::cout << "Game is starting.." << std::endl;
+
 	GameTitle title;
 	title.StartTitle();
-	
+
 	GameMenu menu;
 	exitStatus = menu.StartMenu();
-	
+
 	if (exitStatus == 1) {
  		Engine engine;
 		engine.StartEngine();
+	} else {
+		std::cout << "Game is closing..." << std::endl;
 	}
-	else if (exitStatus == 2) {
-		// lataa pelin
-	}
-	else {	
-		std::cout << "Game is closing...LOL" << std::endl;
-	}	
 
 	std::cout << "Game is closed." << std::endl;
-	
-    /*        *
-             / \
-            /___\
-           ( o o )            * *
-           )  L  (           /   * *
-   ________()(-)()________  /     * * *
- E\| _____ )()()() ______ |/B     * * *
-   |/      ()()()(       \|      * * * *
-           | )() |
-           /     \
-          / *  *  \
-         /   *  *  \
-        / *_  *  _  \
-	*/
+
 }
