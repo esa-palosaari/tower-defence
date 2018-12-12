ec27cdcbb7626dd32bbfa716dc294c9d42da6c87
Katariina Tuovinen <Kata@dhcp-87-250.eduroam.aalto.fi>
2018-12-12 10:49:27 +0200

    comments added

--- include/GameMenu.hpp
+++ include/GameMenu.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <fstream>
 
-class Sores{
+class topScores {	// Class for top scores in game
 
 public:
 	std::string name;
@@ -19,39 +19,34 @@ public:
 	}
 };
 
-bool showTopScore();
-
-void showHiscores();
-
 class GameMenu{
 public:
-    GameMenu();
-	//~GameMenu();
+		GameMenu();
     int StartMenu();
-	bool showTopScore();
+		bool showTopScore();	// is it needed?
     int manageEvents();
-	void StartScores();
-	void showHighScores();
+		void StartScores();
+		void showHighScores();
     void render();
-	void SetToText(sf::Text& text, float x, float y, sf::Font font, int size);
-	std::vector<Sores> topScoresVec; //Existing top scores
-	std::vector<sf::Text> topScores; //Renewed vector
+		void SetToText(sf::Text& text, float x, float y, sf::Font font, int size);
+		std::vector<Sores> topScoresVec; // Existing top scores
+		std::vector<sf::Text> topScores; // Renewed vector
 private:
-	sf::RenderWindow nWindow;
-	sf::Font Font;
-	sf::Text MenuGameButton;
-  	sf::Text StartGameButton;
-	sf::Text LoadGameButton;
-	sf::Text ExitGameButton;
-	sf::Text TopScoreHL;
-	int exitStatus;
-	sf::Sprite title;
-	sf::Texture titleTexture;
-	sf::Music music;
-	bool MenuOn = true;
-	bool topScoresInit = false;
-	sf::Clock showTopScoreClk;
-	sf::SoundBuffer Click;
-	sf::Sound ClickSound;
+		sf::RenderWindow nWindow;
+		sf::Font Font;
+		sf::Text MenuGameButton;
+	  sf::Text StartGameButton;
+		sf::Text LoadGameButton;
+		sf::Text ExitGameButton;
+		sf::Text TopScoreHL;
+		int exitStatus;
+		sf::Sprite title;
+		sf::Texture titleTexture;
+		sf::Music music;
+		bool MenuOn = true;
+		bool topScoresInit = false;
+		sf::Clock showTopScoreClk;
+		sf::SoundBuffer Click;
+		sf::Sound ClickSound;
 };
 #endif
