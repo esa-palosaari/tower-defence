348304127b32aafa90e2988434751df7b13f14e9
Katariina Tuovinen <Kata@dhcp-87-250.eduroam.aalto.fi>
2018-12-12 11:15:44 +0200

    comments added

--- include/Projectile.hpp
+++ include/Projectile.hpp
@@ -7,41 +7,34 @@
 class Projectile{
 public:
     Projectile(int dmg, float x, float y, Types::NPC Type, int targetId);       //Constructor
-    void InitializeTexture();   //Initializes textures.
+    void InitializeTexture();
     bool isBlownUp();
-    virtual void Move(sf::Time elapsedTime, std::vector<Enemy>& enemies);   //Move-method for each projectile
-    virtual int HitTarget(std::vector<Enemy>& enemies) = 0; //HitTarget-function is initialized to zero in case no enemies.
-    
-    //Getters
-    Types::NPC getProjectileType() const;   //Returns Projectile's type (Machinegun, Flame or Rocket
-    sf::Sprite getProjectileSprite() const; //Return projectile's sprite, which is initialized at UserGraphics constructor.
-    
-    //SFML properties
+    virtual void Move(sf::Time elapsedTime, std::vector<Enemy>& enemies);
+    virtual int HitTarget(std::vector<Enemy>& enemies) = 0;  //HitTarget-function is initialized to zero in case no enemies.
+
+    Types::NPC getProjectileType() const;
+    sf::Sprite getProjectileSprite() const;
+
+    // SFML properties
     sf::Clock clock;    //clock
     sf::Sprite AoE;     // Area of effect. This doesn't affect physically objects.
     sf::Sprite ProjectileSprite;    //Sprite for each projectile. Initialized at UserGraphics.
     sf::CircleShape AoD;    //Area of damage. This hurts everything inside it.
-    
+
     //Properties that are needed in public declaration.
-    bool textureInitialize = false; //Info whether texture is initialized or not.
-    bool GraphArea = false; //Contains information whether Aoe is graphde or not.
-    
-    //These are needed to complete rule of five.
-   /* virtual ~Projectile() {}    //Destructor
-    Projectile(const Projectile&) = default;    //Copy constructor
-    Projectile(Projectile&&) = default;          //Move constructor
-    Projectile& operator=(const Projectile&) & = default;  //Copy assignment
-    Projectile& operator=(Projectile&&) & = default;     //move assignment*/
-	sf::Sound ExplosionSound;
-	sf::SoundBuffer Explosion;
+    bool textureInitialize = false; // Info whether texture is initialized or not.
+    bool GraphArea = false; // Contains information whether Aoe is graphde or not.
+
+  	sf::Sound ExplosionSound;
+  	sf::SoundBuffer Explosion;
 
 protected:
-    Types::NPC type;    //type of projectile, specific to each turret
-    int DMG;    //Damage of each projectile
-    float x;    //Position x-coordinate for vector::2f
-    float y;    //Position y-coordinate for vector::2f
-    float Speed=500; //Projectile's speed
-    int TargetID;   //Enemy's identification number;
+    Types::NPC type;    // type of projectile, specific to each turret
+    int DMG;    // Damage of each projectile
+    float x;    // Position x-coordinate for vector::2f
+    float y;    // Position y-coordinate for vector::2f
+    float Speed = 500; //Projectile's speed
+    int TargetID;   // Enemy's identification number;
     bool BlownUp = false; //Used to check whether projectile exists or not.
 };
 
